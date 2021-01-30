3033e4e7de7b8e6c241829eff4e973a32c3aa3a9
Mark-DMC <75181394+Mark-DMC@users.noreply.github.com>
2021-01-30 13:43:03 +0000

    Fixed js contactForm

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+function myFunction() {
+  alert("Hello! Welcome to Sarah Clark's Nutrition Website! Here you will find all the help you need for a better way of living! =)");
+}
 
-// function myFunction() {
-//   alert("Hello! Welcome to Sarah Clark's Nutrition Website! Here you will find all the help you need for a better way of living! =)");
-// }
 
-const message =
-  "Thank you! I will reply as soon as possible!";
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+const message = "Thank you! I will reply as soon as possible!";
+const form = document.getElementById("contactForm")
+  form.addEventListener("submit", function (event) {
+    event.preventDefault()
     alert(message);
   });
