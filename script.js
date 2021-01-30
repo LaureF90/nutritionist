35079641eb60b3df710a37a34060f32574729bcc
LaureF90 <lauredefoestraets@hotmail.com>
2021-01-30 13:21:42 +0100

    no alert msg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function myFunction() {
-  alert("Hello! Welcome to Sarah Clark's Nutrition Website! Here you will find all the help you need for a better way of living! =)");
-}
+// function myFunction() {
+//   alert("Hello! Welcome to Sarah Clark's Nutrition Website! Here you will find all the help you need for a better way of living! =)");
+// }
 
 
 const message =
