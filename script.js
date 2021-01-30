f448da505f2412d7dbf6c8395cca78729fbed635
RicLaranjo <ricardo.laranjo1711@gmail.com>
2021-01-30 13:47:07 +0000

    fix about page last answer, fix responsiveness

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-// function myFunction() {
-//   alert("Hello! Welcome to Sarah Clark's Nutrition Website! Here you will find all the help you need for a better way of living! =)");
-// }
-
 const message =
   "Thank you! I will reply as soon as possible!";
 document
